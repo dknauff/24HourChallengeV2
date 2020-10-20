74b82b856d05312fc27f6fc46dcab73356898bc6
dknauff <dknauffsd@gmail.com>
2020-10-20 14:55:49 -0400

    comment added

---
+++ 24HourChallenge.Services/CommentService.cs
@@ -0,0 +1,61 @@
+﻿using _24HourChallenge.Data;
+using _24HourChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFHourChallenge.Models;
+
+namespace _24HourChallenge.Services
+{
+    public class CommentService
+    {
+        private readonly Guid _userId;
+
+        public CommentService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateComment(CommentCreate model)
+        {
+            var entity =
+                new Comment()
+                {
+                    Text = model.Text,
+                    CommentPostId = model.CommentPostId
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Comments.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CommentListItem> GetComments()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Comments
+                        .Where(e => e.AuthorId == _userId)
+                        .Select(
+                            e =>
+                                new CommentListItem
+                                {
+                                    Id = e.Id,
+                                    Text = e.Text,
+                                    AuthorId = e.AuthorId,
+                                    CommentPostId = e.CommentPostId
+                                }
+                          );
+
+                return query.ToArray();
+
+            }
+        }
+    }
+}
